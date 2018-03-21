f0e5c8e4b05130c1a6e9281acedfb8075c1c2926
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-21 19:59:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,4 +59,13 @@ class Student
     result = DB[:conn].execute(sql, name)[0]
     Student.new(result[0], result[1], result[2])
   end
+
+  def update
+    sql = <<-SQL
+      UPDATE students
+      SET name = ?, album = ?
+      WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.id, self.name, self.grade)
+  end  
 end
