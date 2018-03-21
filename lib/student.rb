961e1feb927ada9f61983c1b8db9dcf91edd60ab
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-21 20:12:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,8 +65,9 @@ class Student
       WHERE name = ?
     SQL
 
-    result = DB[:conn].execute(sql, name)[0]
-    Student.new(result[0], result[1], result[2])
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def update
