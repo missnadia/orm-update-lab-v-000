34bf53624a1f8d92c5b86f478edc9c3039cbf6c0
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-21 20:19:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,11 +62,12 @@ class Student
       SELECT *
       FROM  students
       WHERE name = ?
+      LIMIT 1
     SQL
 
-    sql = "SELECT * FROM songs WHERE name = ?"
-  result = DB[:conn].execute(sql, name)[0]
-  Student.new(result[0], result[1], result[2])
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def update
